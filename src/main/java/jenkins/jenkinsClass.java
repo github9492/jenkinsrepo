a76624a0654d3a0e2c7cc5721d8ea81188524f5a
github@9492 <gkkuswaha1@gmail.com>
2023-03-10 17:39:56 +0530

    gkk

--- src/main/java/jenkins/jenkinsClass.java
+++ src/main/java/jenkins/jenkinsClass.java
@@ -2,9 +2,11 @@ package jenkins;
 
 public class jenkinsClass {
 	public static void main(String arg []) {
+		String browser = System.getProperty("url");
+		System.out.println(browser);
 		System.out.println("hii jenkins");
 		System.out.println("hii jenkins");
-		//hiii
+		
 	}
 
 }
