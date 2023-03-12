e68e19d07b56d234c75de4debbf284bf0c0a5772
github@9492 <gkkuswaha1@gmail.com>
2023-03-12 23:51:14 +0530

    sending the browser from cmd

--- src/main/java/jenkins/jenkinsTest.java
+++ src/main/java/jenkins/jenkinsTest.java
@@ -1,7 +1,10 @@
 package jenkins;
 
-public class jenkinsClass {
-	public static void main(String arg []) {
+import org.testng.annotations.Test;
+
+public class jenkinsTest {
+	@Test
+	public void abc() {
 		String browser = System.getProperty("url");
 		System.out.println(browser);
 		System.out.println("hii jenkins");
